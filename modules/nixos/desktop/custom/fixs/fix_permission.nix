e0b0bd8ec57236b287fd4c2a2f6df6a718f3b3e4
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-03-24 03:10:35 +0800

    feat(cloudflare-warp):add wrapper for cloudflare-warp.warp-svc;add widget for cloudflare-warp

---
+++ modules/nixos/desktop/custom/fixs/fix_permission.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  security.wrappers.warp-svc = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_net_bind_service,cap_net_raw,cap_net_admin,cap_dac_override+p";
+    source = "${pkgs.cloudflare-warp}/bin/warp-svc";
+  };
+}
