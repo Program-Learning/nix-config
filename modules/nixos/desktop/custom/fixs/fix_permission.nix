347d9ec54966f82962c7c4b1e79d5d71aa3c93d8
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-04-06 03:00:44 +0800

    feat: add cloudflare-warp; try to fix wf-recorder

--- modules/nixos/desktop/custom/fixs/fix_permission.nix
+++ modules/nixos/desktop/custom/fixs/fix_permission.nix
@@ -4,10 +4,10 @@
   lib,
   ...
 }: {
-  security.wrappers.warp-svc = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_net_bind_service,cap_net_raw,cap_net_admin,cap_dac_override+p";
-    source = "${pkgs.cloudflare-warp}/bin/warp-svc";
-  };
+  #security.wrappers.warp-svc = {
+  #  owner = "root";
+  #  group = "root";
+  #  capabilities = "cap_net_bind_service,cap_net_raw,cap_net_admin,cap_dac_override+p";
+  #  source = "${pkgs.cloudflare-warp}/bin/warp-svc";
+  #};
 }
