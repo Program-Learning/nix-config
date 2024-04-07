7eaeec0237ef2b9f8d594817dc5dd114c994799d
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-04-07 15:12:07 +0800

    feat: add overlay for lenovo_legion

--- overlays/lenovo-legion/default.nix
+++ overlays/lenovo-legion/default.nix
@@ -2,15 +2,19 @@ _: (_: super: {
   lenovo-legion = super.lenovo-legion.overrideAttrs (oldAttrs @ {
     postInstall ? "",
     postPatch,
-    lib,
+    lib ? super.lib,
     ...
   }: let
     new_postInstall = ''
       ${postInstall}
-      cp $out/share/applications/legion_gui.desktop $out/share/applications/legion_gui_with_cap.desktop
-      sed -i "s@^Exec=pkexec legion_gui@Exec=legion_gui_with_cap@g" $out/share/applications/legion_gui_with_cap.desktop
-      sed -i "s@^Name=Root LenovoLegionLinux@Name=LenovoLegionLinux with capability@g" $out/share/applications/legion_gui_with_cap.desktop
-      cp $out/share/legion_linux /etc/legion_linux
+
+      # This will failed
+      #cp $out/share/applications/legion_gui.desktop $out/share/applications/legion_gui_with_cap.desktop
+      #sed -i "s@^Exec=pkexec legion_gui@Exec=/run/wrappers/bin/legion_gui_with_cap@g" $out/share/applications/legion_gui_with_cap.desktop
+      #sed -i "s@^Name=Root LenovoLegionLinux@Name=LenovoLegionLinux with capability@g" $out/share/applications/legion_gui_with_cap.desktop
+
+      # This should exec by users themselves
+      #cp -r $out/share/legion_linux /etc/legion_linux
       #sed -i "s@^$out/share/legion_linux@/etc/legion_linux@g" $out/bin/fancurve-set
       #sed -i "s@^$out/share/legion_linux@/etc/legion_linux@g" $out/lib/python3.11/site-packages/legion_linux/legion.py
     '';
