3e4204e2215ccf5c4ba4591e016ed70f09a1297c
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-04-07 15:12:43 +0800

    feat: optimize hyprland.conf

--- hosts/idols-y9000k2021h/home.nix
+++ hosts/idols-y9000k2021h/home.nix
@@ -1,14 +1,20 @@
-{
+let
+  intel_card = "/dev/dri/card0";
+  nvidia_card = "/dev/dri/card1";
+in {
   modules.desktop = {
     hyprland = {
-      nvidia = true;
+      nvidia = false;
       settings = {
         # Configure your Display resolution, offset, scale and Monitors here, use `hyprctl monitors` to get the info.
         #   highres:      get the best possible resolution
-        #   auto:         postition automatically
+        #   auto:         position automatically
         #   1.5:          scale to 1.5 times
         #   bitdepth,10:  enable 10 bit support
         monitor = "eDP-1,2560x1600@60,0x0,1";
+        env = [
+          #         "WLR_DRM_DEVICES,${nvidia_card}:${nvidia_card}"
+        ];
       };
     };
     i3.nvidia = true;
@@ -81,5 +87,10 @@
         IdentityFile ~/.ssh/y9000k2021h_id_ed25519
         IdentitiesOnly yes
     '';
+  };
+
+  xdg.configFile = {
+    "gpus/intel_card".source = /. + "/dev/dri/by-path/pci-0000:00:02.0-card";
+    "gpus/nvidia_card".source = /. + "/dev/dri/by-path/pci-0000:01:00.0-card";
   };
 }
