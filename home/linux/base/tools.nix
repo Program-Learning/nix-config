8323deecbfe03084e655c17a526ca760c4007eff
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-04-10 00:03:55 +0800

    fix: etcher has been removed

--- home/linux/base/tools.nix
+++ home/linux/base/tools.nix
@@ -6,7 +6,7 @@
     wireguard-tools # manage wireguard vpn manually, via wg-quick
 
     ventoy # create bootable usb
-    etcher # create bootable usb
+    pkgs-stable.etcher # create bootable usb
     virt-viewer # vnc connect to VM, used by kubevirt
     baobab # Graphical disk usage analyzer
   ];
