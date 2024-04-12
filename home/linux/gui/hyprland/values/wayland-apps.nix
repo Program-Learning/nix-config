67be684de00202d4fc71c9964a903bf8895b6e4e
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-04-12 22:52:40 +0800

    feat(chrome): temporary switch to x11 for better gpu acceleration

--- home/linux/gui/hyprland/values/wayland-apps.nix
+++ home/linux/gui/hyprland/values/wayland-apps.nix
@@ -45,7 +45,9 @@
       # https://wiki.archlinux.org/title/Chromium#Native_Wayland_support
       commandLineArgs = [
         "--ozone-platform-hint=auto"
-        "--ozone-platform=wayland"
+        #"--ozone-platform=wayland"
+        # temporary use x11 for gpu acceleration
+        "--ozone-platform=x11"
         # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
         # (only supported by chromium/chrome at this time, not electron)
         "--gtk-version=4"
