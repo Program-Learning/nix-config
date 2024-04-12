8081db790c9474309ebf71605c70293b860f8a05
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-04-13 01:21:09 +0800

    SnapShot

--- flake.nix
+++ flake.nix
@@ -245,8 +245,8 @@
     # my private secrets, it's a private repository, you need to replace it with your own.
     # use ssh protocol to authenticate via ssh-agent/ssh-key, and shallow clone to save time
     mysecrets = {
-      url = "git+ssh://git@github.com/DataEraserC/nix-secrets.git?shallow=1";
-      # url = "git+file:////home/nixos/Documents/code/nix-config/secrets?shallow=1";
+      # url = "git+ssh://git@github.com/DataEraserC/nix-secrets.git?shallow=1";
+      url = "git+file:////home/nixos/Documents/code/nix-config/secrets?shallow=1";
       flake = false;
     };
 
