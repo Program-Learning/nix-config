4d1a9045b5bfcfc2e27cc3c07b8e227b0cb95989
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-04-17 01:53:37 +0800

    feat: enable FishIntegration for yazi

--- home/base/core/yazi.nix
+++ home/base/core/yazi.nix
@@ -11,6 +11,7 @@
     # Changing working directory when exiting Yazi
     enableBashIntegration = true;
     enableNushellIntegration = true;
+    enableFishIntegration = true;
   };
 
   xdg.configFile."yazi/theme.toml".source = "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-yazi}/mocha.toml";
