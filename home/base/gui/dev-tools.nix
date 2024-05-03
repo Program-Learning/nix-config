7eb44c80b4ec5fc6dc6ce15ab8139b6d62fe1473
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-05-03 16:20:39 +0800

    feat(package addition): add whistle,charles

--- home/base/gui/dev-tools.nix
+++ home/base/gui/dev-tools.nix
@@ -4,6 +4,8 @@
     dbeaver
 
     mitmproxy # http/https proxy tool
+    whistle # HTTP, HTTP2, HTTPS, Websocket debugging proxy
+    # charles # Web Debugging Proxy
     insomnia # REST client
     wireshark # network analyzer
     hoppscotch # Api Test Tool
