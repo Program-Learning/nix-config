75c57125196eb0bdcae67249f377f193b7eab03e
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-05-20 00:32:59 +0800

    fix: changing nix-on-droid's shell to fish broken sshd

--- modules/droid/font.nix
+++ modules/droid/font.nix
@@ -6,6 +6,7 @@
   # terminal.font = "${pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];}}/share/fonts/truetype/NerdFonts/JetBrainsMonoNerdFont-Regular.ttf";
 
   # Test Required
-  user.shell = "${pkgs.fish}/bin/fish";
+  # cause sshd broken
+  # user.shell = "${pkgs.fish}/bin/fish";
   terminal.font = "${pkgs.nerdfonts.override {fonts = ["FiraCode"];}}/share/fonts/truetype/NerdFonts/FiraCodeNerdFont-Retina.ttf";
 }
