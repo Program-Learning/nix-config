f8a438d1bd1cf1670d984a6f3ba86e06bda1f98a
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-06-03 16:14:10 +0800

    fix: some substituters is not work

--- modules/base.nix
+++ modules/base.nix
@@ -98,6 +98,7 @@
       # "https://mirror.sjtu.edu.cn/nix-channels/store"
       # status: https://mirrors.ustc.edu.cn/status/
       "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 
       "https://nix-community.cachix.org"
       # my own cache server
