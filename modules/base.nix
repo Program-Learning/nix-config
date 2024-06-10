fd15a3c167fca40737473c88e47690e0315c6f6a
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-06-10 17:39:14 +0800

    feat: enable some mirror

--- modules/base.nix
+++ modules/base.nix
@@ -99,7 +99,7 @@
       # status: https://mirror.sjtu.edu.cn/
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       # status: https://mirrors.ustc.edu.cn/status/
-      # "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 
       "https://nix-community.cachix.org"
