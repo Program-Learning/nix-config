073e0440c0d919539cc074c3502648a3b007f4b6
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-01-05 15:29:10 +0800

    Snapshot

--- home/linux/gui/base/fcitx5/default.nix
+++ home/linux/gui/base/fcitx5/default.nix
@@ -17,6 +17,8 @@
 
   i18n.inputMethod = {
     enabled = "fcitx5";
+    # home manager do not have this option
+    # fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       # for flypy chinese input method
       fcitx5-rime
