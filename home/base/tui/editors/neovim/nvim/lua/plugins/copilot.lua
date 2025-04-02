8a38439ec2010bd12c6d2407313108d8221c913c
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-04-03 01:19:38 +0800

    feat: use upstream bcs upstream has merge that commit

--- home/base/tui/editors/neovim/nvim/lua/plugins/copilot.lua
+++ home/base/tui/editors/neovim/nvim/lua/plugins/copilot.lua
@@ -1,10 +1,8 @@
 -- Custom copilot-lua to enable filetypes: markdown
 return {
-  -- "zbirenbaum/copilot.lua",
+  "zbirenbaum/copilot.lua",
 
-  -- Fix https://github.com/zbirenbaum/copilot.lua/pull/336
-  "ryan4yin/copilot.lua",
-  branch = "fix_issue_330",
+  branch = "master",
   opts = function(_, opts)
     opts.filetypes = {
       yaml = true,
