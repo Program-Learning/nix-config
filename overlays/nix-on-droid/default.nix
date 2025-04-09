92dabecb7211b25f05559014aae3b5d5b2089e8f
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-04-09 19:07:43 +0800

    fix a bug

--- overlays/nix-on-droid/default.nix
+++ overlays/nix-on-droid/default.nix
@@ -1,17 +1,22 @@
-args:
-if args."nix-on-droid" == true
-then
-  # execute and import all overlay files in the current directory with the given args
-  (builtins.map
-    (f: (import (./. + "/${f}") args)) # execute and import the overlay file
-    
-    (builtins.filter # find all overlay files in the current directory
+args: let
+  is_nix_on_droid =
+    if args ? "nix-on-droid"
+    then args."nix-on-droid"
+    else false;
+in
+  if is_nix_on_droid == true
+  then
+    # execute and import all overlay files in the current directory with the given args
+    (builtins.map
+      (f: (import (./. + "/${f}") args)) # execute and import the overlay file
       
-      (
-        f:
-          f
-          != "default.nix" # ignore default.nix
-          && f != "README.md" # ignore README.md
-      )
-      (builtins.attrNames (builtins.readDir ./.))))
-else []
+      (builtins.filter # find all overlay files in the current directory
+        
+        (
+          f:
+            f
+            != "default.nix" # ignore default.nix
+            && f != "README.md" # ignore README.md
+        )
+        (builtins.attrNames (builtins.readDir ./.))))
+  else []
