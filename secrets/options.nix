34faec1ec32366a3ef95f7f9de833405b8662e5a
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-05-09 11:36:11 +0800

    snapshot

---
+++ secrets/options.nix
@@ -0,0 +1,15 @@
+{lib, ...}:
+with lib; {
+  options.modules.secrets = {
+    desktop.enable = mkEnableOption "NixOS Secrets for Desktops";
+
+    server.network.enable = mkEnableOption "NixOS Secrets for Network Servers";
+    server.application.enable = mkEnableOption "NixOS Secrets for Application Servers";
+    server.operation.enable = mkEnableOption "NixOS Secrets for Operation Servers(Backup, Monitoring, etc)";
+    server.kubernetes.enable = mkEnableOption "NixOS Secrets for Kubernetes";
+    server.webserver.enable = mkEnableOption "NixOS Secrets for Web Servers(contains tls cert keys)";
+    server.storage.enable = mkEnableOption "NixOS Secrets for HDD Data's LUKS Encryption";
+
+    impermanence.enable = mkEnableOption "whether use impermanence and ephemeral root file system";
+  };
+}
