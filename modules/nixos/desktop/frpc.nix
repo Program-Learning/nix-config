a85ae17922d3a00b3b4defe8dc28d883aec863a4
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-05-10 18:45:08 +0800

    fix: i forgot role of frp

--- modules/nixos/desktop/frpc.nix
+++ modules/nixos/desktop/frpc.nix
@@ -4,6 +4,7 @@
     modules.frp.instances = {
       aliyun2025 = {
         enable = true;
+        role = "client";
         configFile = "${config.age.secrets."frp_aliyun2025.toml".path}";
       };
     };
