32e21334bcadeb38f88ae7a75f1102695f869972
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-07-17 20:25:59 +0800

    snapshot

--- home/base/tui/editors/neovim/default.nix
+++ home/base/tui/editors/neovim/default.nix
@@ -17,6 +17,8 @@ let
     vdiff = "nvim -d";
   };
   # the path to nvim directory
+  # to make this symlink work, we need to git clone this repo to your home directory.
+  # configPath = "${config.home.homeDirectory}/nix-config/home/base/tui/editors/neovim/nvim";
   # NOTE: to make this symlink work, we need to git clone this repo to your home directory which is writable (any other method may first cp to nix store then eval).
   configPath = mylib.mklinkRelativeToRoot config "home/base/tui/editors/neovim/nvim";
 in {
