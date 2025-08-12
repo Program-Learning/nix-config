8a26ec05924b6170f38d59fd941e6ebecf1bdd4c
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-08-13 00:09:36 +0800

    keep DBeaverData

--- hosts/wsl-r9000p2025/preservation.nix
+++ hosts/wsl-r9000p2025/preservation.nix
@@ -315,6 +315,12 @@ in
 
         # Mayuri Spec
 
+        # ======================================
+        # Apps
+        # ======================================
+
+        ".local/share/DBeaverData/"
+
         (makeDirRW ".wine")
         (makeDirRW ".minecraft")
         (makeDirRW "GOG Games")
