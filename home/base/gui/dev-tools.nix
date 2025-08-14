31326e683cfdca59f072d2b9d8034d0f944814bf
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-08-14 08:38:38 +0800

    chore: add jetbrain ides

--- home/base/gui/dev-tools.nix
+++ home/base/gui/dev-tools.nix
@@ -1,4 +1,40 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  nur-DataEraserC,
+  ...
+}:
+let
+  getMajorMinorVersion =
+    version: builtins.concatStringsSep "." (lib.take 2 (lib.splitString "." version));
+  jetbra = pkgs.fetchFromGitHub {
+    owner = "LostAttractor";
+    repo = "jetbra";
+    rev = "94585581c360862eab1843bf7edd8082fdf22542";
+    sha256 = "sha256-9jeiF9QS4MCogIowu43l7Bqf7dhs40+7KKZML/k1oWo=";
+  };
+
+  vmoptions = ''
+    --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
+    --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED
+
+    -javaagent:${nur-DataEraserC.packages.${pkgs.system}.ja-netfilter}/ja-netfilter.jar=jetbrains
+  '';
+  # -javaagent:${jetbra}/ja-netfilter.jar=jetbrains
+  # -javaagent:/home/nixos/Documents/.jetbra-free/static/ja-netfilter/ja-netfilter.jar=jetbrains
+  postFixUpPatchFunction =
+    productDir: vmoptionsFileName:
+    old@{ postFixup, ... }:
+    {
+      postFixup = ''
+        ${if postFixup != null then old.postFixup else ""}
+
+        # add ja-netfilter
+        cd $out/${productDir}
+        echo '${vmoptions}' >> ${vmoptionsFileName}
+      '';
+    };
+in
 {
   home.packages =
     with pkgs;
@@ -33,8 +69,46 @@
     ])
     ++ [
       jetbrains.idea-community
-      # jetbrains.idea-ultimate
+
+      jetbrains.idea-ultimate
+      jetbrains.rust-rover
+      jetbrains.pycharm-professional
+      jetbrains.goland
+      jetbrains.webstorm
+      jetbrains.clion
+
+      # (jetbrains.idea-ultimate.overrideAttrs {vmopts = vmoptions;})
+      # (jetbrains.rust-rover.overrideAttrs {vmopts = vmoptions;})
+      # (jetbrains.pycharm-professional.overrideAttrs {vmopts = vmoptions;})
+      # (jetbrains.goland.overrideAttrs {vmopts = vmoptions;})
+      # (jetbrains.webstorm.overrideAttrs {vmopts = vmoptions;})
+      # (jetbrains.clion.overrideAttrs {vmopts = vmoptions;})
+
+      # (jetbrains.idea-ultimate.overrideAttrs
+      # (postFixUpPatchFunction "idea-ultimate/bin" "idea64.vmoptions"))
+      # (jetbrains.rust-rover.overrideAttrs
+      #   (postFixUpPatchFunction "rust-rover/bin" "rustrover64.vmoptions"))
+      # (jetbrains.pycharm-professional.overrideAttrs
+      #   (postFixUpPatchFunction "pycharm/bin" "pycharm64.vmoptions"))
+      # (jetbrains.goland.overrideAttrs
+      #   (postFixUpPatchFunction "goland/bin" "goland64.vmoptions"))
+      # (jetbrains.webstorm.overrideAttrs
+      #   (postFixUpPatchFunction "webstorm/bin" "webstorm64.vmoptions"))
+      # (jetbrains.clion.overrideAttrs
+      #   (postFixUpPatchFunction "clion/bin" "clion64.vmoptions"))
       # eclipses.eclipse-sdk
       eclipses.eclipse-jee
     ];
+  xdg.configFile."JetBrains/IntelliJIdea${getMajorMinorVersion pkgs.jetbrains.idea-ultimate.version}/idea64.vmoptions".text =
+    vmoptions;
+  xdg.configFile."JetBrains/PyCharm${getMajorMinorVersion pkgs.jetbrains.pycharm-professional.version}/pycharm64.vmoptions".text =
+    vmoptions;
+  xdg.configFile."JetBrains/CLion${getMajorMinorVersion pkgs.jetbrains.clion.version}/clion64.vmoptions".text =
+    vmoptions;
+  xdg.configFile."JetBrains/RustRover${getMajorMinorVersion pkgs.jetbrains.rust-rover.version}/rustrover64.vmoptions".text =
+    vmoptions;
+  xdg.configFile."JetBrains/GoLand${getMajorMinorVersion pkgs.jetbrains.goland.version}/goland64.vmoptions".text =
+    vmoptions;
+  xdg.configFile."JetBrains/WebStorm${getMajorMinorVersion pkgs.jetbrains.webstorm.version}/webstorm64.vmoptions".text =
+    vmoptions;
 }
