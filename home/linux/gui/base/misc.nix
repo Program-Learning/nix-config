61df55486d3a65179bab435ad8d0cc85a532d29e
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-09-13 00:23:42 +0800

    snapshot

--- home/linux/gui/base/misc.nix
+++ home/linux/gui/base/misc.nix
@@ -89,5 +89,6 @@
 
   # allow fontconfig to discover fonts and configurations installed through home.packages
   # Install fonts at system-level, not user-level
-  fonts.fontconfig.enable = false;
+  # NOTE: temporarily set it to true
+  fonts.fontconfig.enable = true;
 }
