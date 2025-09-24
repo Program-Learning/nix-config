9305cc981fce52bd4c42eb4f1e7292b4fca43692
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-09-24 17:33:41 +0800

    snapshot

--- home/linux/gui/gnome-wayland/values/gnome.nix
+++ home/linux/gui/gnome-wayland/values/gnome.nix
@@ -18,5 +18,12 @@ in
         gsconnect.extensionUuid
       ];
     };
+    settings."org/gnome/mutter" = {
+      experimental-features = [
+        "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+        "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+        "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+      ];
+    };
   };
 }
